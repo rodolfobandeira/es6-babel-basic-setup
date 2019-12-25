a5aca42e04810c45fdf2e80c6d202b5e2b018fb9
rodolfobandeira <contato@rodolfobandeira.com>
2019-12-25 10:55:20 -0500

    Upgrade packages

--- app/example1.js
+++ app/example1.js
@@ -1,10 +1,14 @@
-'use strict';
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+"use strict";
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Example1 = function () {
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Example1 =
+/*#__PURE__*/
+function () {
   function Example1() {
     _classCallCheck(this, Example1);
 
@@ -12,14 +16,14 @@ var Example1 = function () {
   }
 
   _createClass(Example1, [{
-    key: '_myPrivateMethod',
+    key: "_myPrivateMethod",
     value: function _myPrivateMethod() {
       ['a', 'b', 'c', 'd'].some(function (letra) {
         return letra == 'b';
       });
     }
   }], [{
-    key: 'myStaticMethod',
+    key: "myStaticMethod",
     value: function myStaticMethod() {
       console.log("staticMethod");
     }
